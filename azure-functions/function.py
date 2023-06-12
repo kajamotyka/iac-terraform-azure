b6698b27191ad501d410d456f2953dc0321eb8c8
kajamotyka <kaja.motyka@hotmail.com>
2023-06-13 00:28:48 +0200

    poprawiony format daty

--- azure-functions/function.py
+++ azure-functions/function.py
@@ -3,5 +3,5 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:    
-    now = datetime.datetime.now().strftime("%Y-%m-%D %H:%M:%S")
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return func.HttpResponse(f"Witaj, aktualny czas {now}!")
